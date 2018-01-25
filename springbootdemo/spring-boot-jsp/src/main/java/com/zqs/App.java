851067f58a250f427d9203718418ed2e6afa257b
EricZhang1215 <879691755@qq.com>
2018-01-25 20:43:32 +0800

    .gitignore

--- springbootdemo/spring-boot-jsp/src/main/java/com/zqs/App.java
+++ springbootdemo/spring-boot-jsp/src/main/java/com/zqs/App.java
@@ -1,4 +1,4 @@
-package com.kfit;
+package com.zqs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springbootdemo/spring-boot-jsp/src/main/java/com/zqs/HelloController.java
+++ springbootdemo/spring-boot-jsp/src/main/java/com/zqs/HelloController.java
@@ -1,4 +1,4 @@
-package com.kfit;
+package com.zqs;
 
 import java.util.Map;
 
